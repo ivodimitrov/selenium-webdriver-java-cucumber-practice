825f30462fc81d59f77ede8faafb65ba5e356088
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-07-18 19:37:25 +0300

    Refactored HomePage.

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -1,6 +1,9 @@
 package pageobjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import utils.DriverFactory;
@@ -13,7 +16,10 @@ public class HomePage extends DriverFactory {
      */
     // Amazon Logo
     // private By amazonLogo = By.xpath(".//*[@id='nav-logo']/a[1]/span[1]");
-    private By amazonLogo = By.cssSelector("span.nav-logo-base");
+    // private By amazonLogo = By.cssSelector("span.nav-logo-base");
+    @FindBy(css = "span.nav-logo-base")
+    private WebElement amazonLogo;
+
     // Dropdown Section
     // private By searchDropdown = By.xpath(".//*[@id='searchDropdownBox']");
     private By searchDropdown = By.cssSelector("#searchDropdownBox");
@@ -21,13 +27,19 @@ public class HomePage extends DriverFactory {
     // private By searchFieldTextbox = By.xpath(".//*[@id='twotabsearchtextbox']");
     private By searchFieldTextbox = By.cssSelector("#twotabsearchtextbox");
 
+    public HomePage() {
+        PageFactory.initElements(driver, this);
+    }
+
     /**
      * All functions related to behavior will follow now
      */
     // Verify that home page is correct and opened
     public void verifyHomePageIsDisplayed(String webPage) {
-        waitVar.until(ExpectedConditions.presenceOfElementLocated(amazonLogo));
-        assertTrue("Home page is not displayed", driver.findElement(amazonLogo).isDisplayed());
+        // waitVar.until(ExpectedConditions.presenceOfElementLocated(amazonLogo));
+        waitVar.until(ExpectedConditions.visibilityOf(amazonLogo));
+        // assertTrue("Home page is not displayed", driver.findElement(amazonLogo).isDisplayed());
+        assertTrue("Home page is not displayed", amazonLogo.isDisplayed());
         assertEquals("Home page is not correct", webPage, driver.getTitle());
     }
 
