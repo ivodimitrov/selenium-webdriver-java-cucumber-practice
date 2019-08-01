5d013fc621bb848d4d9289a4232103efa22fe63f
Ivo Dimitrov <ivopdimitrov@gmail.com>
2019-08-01 18:10:26 +0300

    Updated ChromeDriver.

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -8,12 +8,23 @@ import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
 
-    protected static WebDriver driver = null;
+    protected static WebDriver driver;
     protected static WebDriverWait waitVar = null;
 
     public void setUp() {
+//        System.setProperty("webdriver.chrome.driver",
+//                "C:\\Users\\Ivo.Dimitrov\\IdeaProjects\\tests-at-amazon\\src\\test\\resources\\drivers\\chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver",
+                "./src/test/resources/drivers/chromedriver.exe");
+
+        // Initialize new WebDriver session
+        System.out.println("Starting driver...");
         driver = new ChromeDriver();
+
         // driver = new FirefoxDriver();
+        System.out.println("Started driver.");
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         String baseURL = "https://www.amazon.co.uk/";
